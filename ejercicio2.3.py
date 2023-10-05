0451807b7d5ecb1f8b37430fa01804694e32f9d9
jdiabus511 <jadiaz@iesrafaelalberti.es>
2023-10-05 16:19:13 +0200

    ejercicio3

---
+++ ejercicio2.3.py
@@ -0,0 +1,15 @@
+ancho=17
+alto=12.0
+
+ejemplo1=(ancho/2)
+"""El resultado sería 8.5"""
+ejemplo2=(ancho//2)
+"""El resultado sería 8"""
+ejemplo3=(alto/3)
+"""El resultado sería 4.0"""
+ejemplo4=(1+2*5)
+"""El resultado sería 11"""
+print(ejemplo1)
+print(ejemplo2)
+print(ejemplo3)
+print(ejemplo4)
