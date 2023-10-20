82c9b314ed2e7441c715b7ea4e04aeee728540ba
jdiabus511 <jadiaz@iesrafaelalberti.es>
2023-10-20 21:30:13 +0200

    piramide

---
+++ jueginho.py
@@ -0,0 +1,20 @@
+"""
+un programa que pida un numero entre el menos 100 y 100. Si no está entre esos valores repite hasta que sea.
+"""
+def jueguito(numer:int):
+    abs (numer)
+    cont=numer
+    serie=f"serie {numer} => "
+    while (cont!=0):
+        while ((cont+1)!=0):
+            numer=0
+            serie=serie+
+        print(f"{numer} ")
+        cont-=1
+return serie
+
+num=int(input("Introduce un numero entre -100 y 100: "))
+while num<(-100) or num>100:
+    print("¡He dicho entre -100 y 100!")
+    num=int(input("Introduce un numero entre -100 y 100: "))
+
